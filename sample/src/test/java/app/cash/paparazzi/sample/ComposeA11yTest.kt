f95d0152a478f74ceaddb5e2ef16af37f3963be9
Gasper Kojek <gkojek@gradle.com>
2024-07-12 16:04:27 +0200

    Initial commit - more or less copied from the paparazzi repo /sample and whatever else was needed

---
+++ sample/src/test/java/app/cash/paparazzi/sample/ComposeA11yTest.kt
@@ -0,0 +1,154 @@
+package app.cash.paparazzi.sample
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.selection.toggleable
+import androidx.compose.material.Button
+import androidx.compose.material.Checkbox
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.role
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.stateDescription
+import androidx.compose.ui.unit.dp
+import app.cash.paparazzi.DeviceConfig
+import app.cash.paparazzi.Paparazzi
+import app.cash.paparazzi.accessibility.AccessibilityRenderExtension
+import org.junit.Rule
+import org.junit.Test
+
+class ComposeA11yTest {
+  @get:Rule
+  val paparazzi = Paparazzi(
+    deviceConfig = DeviceConfig.PIXEL,
+    renderExtensions = setOf(AccessibilityRenderExtension())
+  )
+
+  @Test
+  fun compositeItems() {
+    paparazzi.snapshot {
+      Column {
+        Row(
+          Modifier
+            .toggleable(
+              value = true,
+              role = Role.Checkbox,
+              onValueChange = { }
+            )
+            .fillMaxWidth()
+        ) {
+          Text("Option", Modifier.weight(1f))
+          Checkbox(checked = true, onCheckedChange = null)
+        }
+        Box(
+          Modifier
+            .align(Alignment.CenterHorizontally)
+            .clickable(onClickLabel = "On Click Label") { }
+        )
+        Row(modifier = Modifier.semantics(mergeDescendants = true) {}) {
+          Image(
+            imageVector = Icons.Filled.Add,
+            contentDescription = null // decorative
+          )
+          Column(modifier = Modifier.semantics(mergeDescendants = true) {}) {
+            Text("Text")
+            Text(
+              text = "more text",
+              modifier = Modifier.semantics { contentDescription = "custom description" }
+            )
+            Column(modifier = Modifier.semantics(mergeDescendants = true) {}) {
+              Text("Nested text")
+              Text(
+                text = "more text",
+                modifier = Modifier.semantics { contentDescription = "custom description" }
+              )
+            }
+          }
+        }
+      }
+    }
+  }
+
+  @Test
+  fun buttonStates() {
+    paparazzi.snapshot {
+      LazyVerticalGrid(columns = GridCells.Fixed(2)) {
+        item {
+          Button(
+            modifier = Modifier.padding(24.dp),
+            onClick = {}
+          ) {
+            Text("01")
+          }
+        }
+        item {
+          Button(
+            modifier = Modifier
+              .padding(24.dp),
+            onClick = {},
+            enabled = false
+          ) {
+            Text("02")
+          }
+        }
+        item {
+          Button(
+            modifier = Modifier
+              .padding(24.dp)
+              .clickable(onClickLabel = "Explore") {},
+            onClick = {}
+          ) {
+            Text("03")
+          }
+        }
+        item {
+          Button(
+            modifier = Modifier
+              .padding(24.dp)
+              .semantics { stateDescription = "State" },
+            onClick = {}
+          ) {
+            Text("04")
+          }
+        }
+        item {
+          Button(
+            modifier = Modifier
+              .padding(24.dp)
+              .semantics { role = Role.DropdownList },
+            onClick = {}
+          ) {
+            Text("05")
+          }
+        }
+        item {
+          Button(
+            modifier = Modifier
+              .padding(24.dp)
+              .clickable(onClickLabel = "Explore") {}
+              .semantics {
+                stateDescription = "State"
+                role = Role.RadioButton
+              },
+            onClick = {},
+            enabled = false
+          ) {
+            Text("06")
+          }
+        }
+      }
+    }
+  }
+}
---
+++ sample/src/test/java/app/cash/paparazzi/sample/TestParameterInjectorTest.kt
@@ -0,0 +1,58 @@
+package app.cash.paparazzi.sample
+
+import android.widget.LinearLayout
+import app.cash.paparazzi.DeviceConfig
+import app.cash.paparazzi.Paparazzi
+import app.cash.paparazzi.sample.databinding.KeypadBinding
+import com.android.resources.ScreenOrientation.LANDSCAPE
+import com.google.testing.junit.testparameterinjector.TestParameter
+import com.google.testing.junit.testparameterinjector.TestParameterInjector
+import com.google.testing.junit.testparameterinjector.TestParameterValuesProvider
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(TestParameterInjector::class)
+class TestParameterInjectorTest(
+  @TestParameter config: Config
+) {
+  enum class Config(
+    val deviceConfig: DeviceConfig
+  ) {
+    NEXUS_4(deviceConfig = DeviceConfig.NEXUS_4),
+    NEXUS_5(deviceConfig = DeviceConfig.NEXUS_5),
+    NEXUS_5_LAND(deviceConfig = DeviceConfig.NEXUS_5.copy(orientation = LANDSCAPE))
+  }
+
+  enum class Theme(val themeName: String) {
+    LIGHT("android:Theme.Material.Light"),
+    LIGHT_NO_ACTION_BAR("android:Theme.Material.Light.NoActionBar")
+  }
+
+  object AmountProvider : TestParameterValuesProvider() {
+    override fun provideValues(context: Context): List<String> = listOf("\$1.00", "\$2.00", "\$5.00", "\$10.00")
+  }
+
+  @get:Rule
+  val paparazzi = Paparazzi(deviceConfig = config.deviceConfig)
+
+  @Test
+  fun simple() {
+    val launch = paparazzi.inflate<LinearLayout>(R.layout.launch)
+    paparazzi.snapshot(launch)
+  }
+
+  @Test
+  fun simpleWithTheme(@TestParameter theme: Theme) {
+    paparazzi.unsafeUpdateConfig(theme = theme.themeName)
+    val launch = paparazzi.inflate<LinearLayout>(R.layout.launch)
+    paparazzi.snapshot(launch)
+  }
+
+  @Test
+  fun amountProviderTest(@TestParameter(valuesProvider = AmountProvider::class) amount: String) {
+    val binding = KeypadBinding.inflate(paparazzi.layoutInflater)
+    binding.amount.text = amount
+    paparazzi.snapshot(binding.root)
+  }
+}
